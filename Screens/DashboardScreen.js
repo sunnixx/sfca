10b0892b66a9c857d6a912897d2b99d6bfa1aa4a
sunnixx <sumair.hamza@gmail.com>
2018-11-12 20:38:37 +0500

    account kit enabled and login auth working

--- Screens/DashboardScreen.js
+++ Screens/DashboardScreen.js
@@ -1,15 +1,35 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React, { Component } from 'react';
+import { AsyncStorage } from 'react-native';
+import { Container, Content, Button, Text } from 'native-base';
+import RNAccountKit from 'react-native-facebook-account-kit';
 
 class DashboardScreen extends Component {
     constructor(props) {
         super(props);
+
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    handleLogout() {
+        RNAccountKit.logout()
+            .then(() => {
+                AsyncStorage.removeItem('token')
+                    .then(() => {
+                        this.props.navigation.navigate('Login');
+                    })
+            })
     }
 
     render() {
-        <View>
-            <Text>Welcome to Dashboard</Text>
-        </View>
+        return (
+            <Container>
+                <Content>
+                    <Button onPress={this.handleLogout}>
+                        <Text>Logout</Text>
+                    </Button>
+                </Content>
+            </Container>
+        )
     }
 }
 
--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,193 +1,86 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Container, Content, Text, Button } from 'native-base';
+import { AsyncStorage } from 'react-native';
+import RNAccountKit from 'react-native-facebook-account-kit';
 
-import { StyleSheet, Text, View, TouchableOpacity, Switch } from "react-native";
+class LoginScreen extends Component {
 
-import AccountKit, {
-  LoginButton,
-  Color,
-  StatusBarStyle
-} from "react-native-facebook-account-kit";
+  constructor(props) {
+    super(props);
 
-class AccountKitSample extends Component {
-  state = {
-    authToken: null,
-    loggedAccount: null
-  };
+    this.handleSMS = this.handleSMS.bind(this);
+    this.handleEmail = this.handleEmail.bind(this);
+    this.checkIfUserIsLogged = this.checkIfUserIsLogged.bind(this);
 
-  componentWillMount() {
-    this.configureAccountKit();
-
-    AccountKit.getCurrentAccessToken()
-      .then(token => {
-        if (token) {
-          AccountKit.getCurrentAccount().then(account => {
-            this.setState({
-              authToken: token,
-              loggedAccount: account
-            });
-          });
-        } else {
-          console.log("No user account logged");
-        }
-      })
-      .catch(e => console.log("Failed to get current access token", e));
-  }
-
-  configureAccountKit() {
-    AccountKit.configure({
+    RNAccountKit.configure({
       responseType: 'code',
-      theme: {
-        //backgroundColor:       Color.rgba(0,120,0,0.1),
-        //buttonBackgroundColor: Color.rgba(0, 153, 0, 1.00),
-        //buttonDisabledBackgroundColor: Color.rgba(100, 153, 0, 0.5),
-        //buttonBorderColor:     Color.rgba(0,255,0,1),
-        //buttonTextColor:       Color.rgba(0,255,0,1),
-        //headerBackgroundColor: Color.rgba(0, 153, 0, 1.00),
-        //headerTextColor:       Color.rgba(0,255,0,1),
-        //headerButtonTextColor: Color.rgba(0,255,0,1),
-        //iconColor:             Color.rgba(0,255,0,1),
-        //inputBackgroundColor:  Color.rgba(0,255,0,1),
-        //inputBorderColor:      Color.hex('#ccc'),
-        //inputTextColor:        Color.hex('#0f0'),
-        //textColor:             Color.hex('#0f0'),
-        //titleColor:            Color.hex('#0f0'),
-        //backgroundImage:       "background.png",
-        //statusBarStyle:        StatusBarStyle.LightContent,
-      },
-      //countryWhitelist: [ "AR", "BR", "US" ],
-      //countryBlacklist: [ "BR" ],
-      //defaultCountry: "AR"
-      initialEmail: "example.com",
-      initialPhoneCountryPrefix: "+92"
-      // initialPhoneNumber: "123-456-7890"
+      defaultCountry: 'PK',
+      initialEmail: 'johndoe@example.com',
+      initialPhoneNumber: '+923'
     });
   }
 
-  onLogin(token) {
-    if (!token) {
-      console.warn("User canceled login");
-      this.setState({});
-    } else {
-      AccountKit.getCurrentAccount().then(account => {
-        this.setState({
-          authToken: token,
-          loggedAccount: account
-        });
-      });
-    }
+  componentDidMount() {
+    this.checkIfUserIsLogged();
   }
 
-  onLoginError(e) {
-    console.log("Failed to login", e);
-  }
-
-  onEmailLoginPressed() {
-    AccountKit.loginWithEmail()
-      .then(token => {
-        this.onLogin(token);
+  checkIfUserIsLogged() {
+    AsyncStorage.getItem('token')
+      .then((token) => {
+        if (!token) {
+          //Do nothing  
+        } else {
+          // console.log("Load dashboard")
+          this.props.navigation.navigate('DrawerStack');
+        }
       })
-      .catch(e => this.onLoginError(e));
   }
 
-  onLogoutPressed() {
-    AccountKit.logout()
-      .then(() => {
-        this.setState({
-          authToken: null,
-          loggedAccount: null
-        });
+  handleEmail() {
+    RNAccountKit.loginWithEmail()
+      .then((token) => {
+        if (!token) {
+          console.log('Login cancelled')
+        } else {
+          AsyncStorage.setItem('token', JSON.stringify(token))
+            .then(() => {
+              // console.log("Load Dashboard");
+              this.props.navigation.navigate('DrawerStack');
+            })
+        }
       })
-      .catch(e => console.log("Failed to logout"));
   }
 
-  renderUserLogged() {
-    const { id, email, phoneNumber } = this.state.loggedAccount;
+  handleSMS() {
+    RNAccountKit.loginWithPhone()
+      .then((token) => {
+        if (!token) {
+          console.log('Login Cancelled')
+        } else {
+          AsyncStorage.setItem('token', token)
+            .then(() => {
+              // console.log("Load Dashboard");
+              this.props.navigation.navigate('DrawerStack');
 
-    return (
-      <View>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => this.onLogoutPressed()}
-        >
-          <Text style={styles.buttonText}>Logout</Text>
-        </TouchableOpacity>
-
-        <Text style={styles.label}>Account Kit Id</Text>
-        <Text style={styles.text}>{id}</Text>
-        <Text style={styles.label}>Email</Text>
-        <Text style={styles.text}>{email}</Text>
-        <Text style={styles.label}>Phone Number</Text>
-        <Text style={styles.text}>
-          {phoneNumber
-            ? `${phoneNumber.countryCode} ${phoneNumber.number}`
-            : ""}
-        </Text>
-      </View>
-    );
-  }
-
-  renderLogin() {
-    return (
-      <View>
-        <LoginButton
-          style={styles.button}
-          type="phone"
-          onLogin={token => this.onLogin(token)}
-          onError={e => this.onLogin(e)}
-        >
-          <Text style={styles.buttonText}>SMS</Text>
-        </LoginButton>
-
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => this.onEmailLoginPressed()}
-        >
-          <Text style={styles.buttonText}>Email</Text>
-        </TouchableOpacity>
-      </View>
-    );
+            })
+        }
+      })
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        {this.state.loggedAccount
-          ? this.renderUserLogged()
-          : this.renderLogin()}
-      </View>
-    );
+      <Container>
+        <Content>
+          <Button onPress={this.handleSMS}>
+            <Text> Login With SMS </Text>
+          </Button>
+          <Button onPress={this.handleEmail}>
+            <Text> Login With Email </Text>
+          </Button>
+        </Content>
+      </Container>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  button: {
-    height: 50,
-    width: 300,
-    backgroundColor: "aqua",
-    marginBottom: 10
-  },
-  buttonText: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  label: {
-    fontSize: 20,
-    textAlign: "center",
-    fontWeight: "bold",
-    marginTop: 20
-  },
-  text: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  }
-});
-
-export default AccountKitSample;
+export default LoginScreen;
